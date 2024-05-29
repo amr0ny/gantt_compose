60d0b1aa17551171bc2f962b1a52d746c74ce7f0
Alex <38862440+amr0ny@users.noreply.github.com>
2024-05-29 10:01:29 +0300

    в

--- app/run.sh
+++ app/run.sh
@@ -3,7 +3,6 @@ mkdir -p /var/www/static
 mkdir -p /var/www/media
 
 chown www-data:www-data /var/log
-chown www-data:www-data /var/www
 
 python manage.py runserver #DO THIS ONLY IN DEBUG MODE
 #uwsgi --strict --ini uwsgi.ini
