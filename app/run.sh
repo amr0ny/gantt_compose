0ac09ea03da5101c331893006c636c42b060019f
Alex <38862440+amr0ny@users.noreply.github.com>
2024-05-29 09:57:09 +0300

    psk

--- app/run.sh
+++ app/run.sh
@@ -5,5 +5,6 @@ mkdir -p /var/www/media
 chown www-data:www-data /var/log
 chown www-data:www-data /var/www
 
+ALLOWED_HOSTS = *
 python manage.py runserver #DO THIS ONLY IN DEBUG MODE
 #uwsgi --strict --ini uwsgi.ini
