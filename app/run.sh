7315c848c0c7cccb3bddf68a9d4265a8cc27da4c
Alex <38862440+amr0ny@users.noreply.github.com>
2024-06-05 15:20:53 +0300

    dfjo

--- app/run.sh
+++ app/run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 mkdir -p /var/www/static
 mkdir -p /var/www/media
-mkdir -p /var/log
-
-chown www-data:www-data /var/log
+mkdir -p /opt/app/logs
+chown www-data:www-data /opt/app/logs
 
 #python manage.py runserver #DO THIS ONLY IN DEBUG MODE
 uwsgi --strict --ini uwsgi.ini
