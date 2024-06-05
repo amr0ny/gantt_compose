3981f8601da500bf89f99276c55584f9f1248a8c
Alex <38862440+amr0ny@users.noreply.github.com>
2024-06-05 14:21:49 +0300

    upd

---
+++ app/config/components/database.py
@@ -0,0 +1,20 @@
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
+        'OPTIONS': {
+            'options': '-c search_path=public,content'
+        }
+    }
+}
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
